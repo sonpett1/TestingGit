d67b82359ee5d609fac51689c18f7c089fee79b2
sonpett1 <sonpett@gmail.com>
2024-01-20 10:49:42 +0100

    Adding some comments

--- TestingGit/Program.cs
+++ TestingGit/Program.cs
@@ -4,9 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        // my name
         Console.WriteLine("My name is Sondre");
     }
-
+    // method that prints numbers up to input number
     static void CountTo(int num)
     {
         for (int i = 1; i <= num; i++)
